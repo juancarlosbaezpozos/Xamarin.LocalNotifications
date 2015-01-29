7d8bc9bed258698da082ddd78a06e4c454f2c1cd
Charly <jbaez@outlook.com>
2015-01-29 14:48:39 -0600

    extensiones
    métodos de extensión

--- LocalNotifications/LocalNotifications.Plugin.iOS/LocalNotifier.cs
+++ LocalNotifications/LocalNotifications.Plugin.iOS/LocalNotifier.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Linq;
 using LocalNotifications.Plugin.Abstractions;
+
 #if __UNIFIED__
 using UIKit;
 using Foundation;
+
 #else
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
@@ -14,47 +17,51 @@ namespace LocalNotifications.Plugin
     /// Implementation of ILocalNotifier for iOS
     /// </summary>
     public class LocalNotifier : ILocalNotifier
-  {
-      private const string NotificationKey = "LocalNotificationKey";
+    {
+        private const string NotificationKey = "LocalNotificationKey";
 
-      /// <summary>
-      /// Notifies the specified notification.
-      /// </summary>
-      /// <param name="notification">The notification.</param>
-      public void Notify(LocalNotification notification)
-      {
-          var nativeNotification = createNativeNotification(notification);
+        /// <summary>
+        /// Notifies the specified notification.
+        /// </summary>
+        /// <param name="notification">The notification.</param>
+        public void Notify(LocalNotification notification)
+        {
+            var nativeNotification = createNativeNotification(notification);
           
-          UIApplication.SharedApplication.ScheduleLocalNotification(nativeNotification);
-      }
+            UIApplication.SharedApplication.ScheduleLocalNotification(nativeNotification);
+        }
 
-      /// <summary>
-      /// Cancels the specified notification identifier.
-      /// </summary>
-      /// <param name="notificationId">The notification identifier.</param>
-      public void Cancel(int notificationId)
-      {
-          var notifications = UIApplication.SharedApplication.ScheduledLocalNotifications;
-          var notification = notifications.Where(n => n.UserInfo.ContainsKey(NSObject.FromObject(NotificationKey)))
+        /// <summary>
+        /// Cancels the specified notification identifier.
+        /// </summary>
+        /// <param name="notificationId">The notification identifier.</param>
+        public void Cancel(int notificationId)
+        {
+            var notifications = UIApplication.SharedApplication.ScheduledLocalNotifications;
+            var notification = notifications.Where(n => n.UserInfo.ContainsKey(NSObject.FromObject(NotificationKey)))
               .FirstOrDefault(n => n.UserInfo[NotificationKey].Equals(NSObject.FromObject(notificationId)));
 
-          if (notification != null)
-          {
-              UIApplication.SharedApplication.CancelLocalNotification(notification);
-          }
-      }
+            if (notification != null)
+            {
+                UIApplication.SharedApplication.CancelLocalNotification(notification);
+            }
+        }
 
-      private UILocalNotification createNativeNotification(LocalNotification notification)
-      {
-          var nativeNotification = new UILocalNotification
-          {
-              AlertAction = notification.Title,
-              AlertBody = notification.Text,
-              FireDate = notification.NotifyTime,
-              UserInfo = NSDictionary.FromObjectAndKey(NSObject.FromObject(notification.Id), NSObject.FromObject(NotificationKey))
-          };
+        private UILocalNotification createNativeNotification(LocalNotification notification)
+        {
+            var nativeNotification = new UILocalNotification
+            {
+                AlertAction = notification.Title,
+                AlertBody = notification.Text,
+                    #if __UNIFIED__
+                FireDate = notification.NotifyTime.DateTimeToNSDate(),
+                    #else
+                FireDate = notification.NotifyTime,
+                    #endif
+                UserInfo = NSDictionary.FromObjectAndKey(NSObject.FromObject(notification.Id), NSObject.FromObject(NotificationKey))
+            };
 
-          return nativeNotification;
-      }
-  }
+            return nativeNotification;
+        }
+    }
 }
